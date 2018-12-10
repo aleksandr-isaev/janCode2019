35a2ba82555168440ad0f5c2c477860fadbec8b8
Michiel-Olieslagers <micholieslagers@gmail.com>
2018-12-10 09:46:51 +0800

    Unlimitted amount of accounts

--- janPrereleaseCode2019/janPrereleaseCode2019/TrackerSystem.vb
+++ janPrereleaseCode2019/janPrereleaseCode2019/TrackerSystem.vb
@@ -1,6 +1,6 @@
 ﻿Public Class TrackerSystem
 
-    Private Accounts(9) As RunTracker
+    Private Accounts(1) As RunTracker
     Private AccountCount As Integer = 0
     Public Sub New()
         Accounts(0) = New RunTracker("Simon", "5000", "fish", True)
@@ -47,7 +47,14 @@
         Dim currentChar As Integer
         Dim addedNumber As Boolean = True
         Dim count As Integer = 0
-
+        Dim oldaccounts(AccountCount - 1) As RunTracker
+        For x = 0 To AccountCount - 1
+            oldaccounts(x) = Accounts(x)
+        Next
+        ReDim Accounts(AccountCount)
+        For x = 0 To AccountCount - 1
+            Accounts(x) = oldaccounts(x)
+        Next
         Console.WriteLine("Enter Your Name")
         Do
             Do
